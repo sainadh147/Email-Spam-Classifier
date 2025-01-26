69c158a2644949b2436d9b9ad6a4bf5a18e256c5
sainadh147 <sainadhbalam@gmail.com>
2025-01-26 18:22:45 +0530

    first commit

--- app.py
+++ app.py
@@ -6,9 +6,6 @@ from nltk.corpus import stopwords
 from nltk.tokenize import word_tokenize
 from nltk.stem import PorterStemmer
 
-nltk.download('punkt')
-nltk.download('stopwords')
-
 ps = PorterStemmer()
 
 # Text transformation function
